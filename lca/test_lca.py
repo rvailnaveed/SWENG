dae65336f6c5286e7e10664b1e7099ac1a156849
Rvail <naveedm@tcd.ie>
2019-09-24 15:20:03 +0100

    test for if no path can be found for x

--- lca/test_lca.py
+++ lca/test_lca.py
@@ -20,15 +20,14 @@ class TestLca(unittest.TestCase):
 
         self.assertTrue(lca_test.ensure_bst(root))
         
+        
     def test_empty_bst(self):
         lca_test = lca()
         root = None
 
         self.assertTrue(lca_test.ensure_bst(root))
 
-    # root = None
-    # x in tree
-    # x not in tree
+
     def test_path_find_empty_bst(self):
         lca_test = lca()
 
@@ -57,6 +56,23 @@ class TestLca(unittest.TestCase):
         self.assertTrue(lca_test.path_find(root, x, path))
 
 
+    def test_path_find_no_x(self):
+        lca_test = lca()
+
+        root = Node(8) 
+        root.left = Node(3) 
+        root.right = Node(10)  
+        root.left.left = Node(1)
+        root.left.right = Node(6)
+        root.left.right.right = Node(7)
+        root.left.right.left = Node(4)
+        root.right.right = Node(14)
+        root.right.right.left = Node(13)
+
+        path = []
+        x = 26
+
+        self.assertFalse(lca_test.path_find(root, x, path))
 
 if __name__ == '__main__':
     unittest.main()
