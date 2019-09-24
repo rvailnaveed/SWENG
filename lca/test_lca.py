abede294deec6b149f9a7908c30645b58fe938fc
Rvail <naveedm@tcd.ie>
2019-09-24 15:17:40 +0100

    test for path_find

--- lca/test_lca.py
+++ lca/test_lca.py
@@ -14,7 +14,6 @@ class TestLca(unittest.TestCase):
         root.left.right = Node(6)
         root.left.right.right = Node(7)
         root.left.right.left = Node(4)
-
         root.right.right = Node(14)
         root.right.right.left = Node(13)
 
@@ -24,8 +23,38 @@ class TestLca(unittest.TestCase):
     def test_empty_bst(self):
         lca_test = lca()
         root = None
-        
+
         self.assertTrue(lca_test.ensure_bst(root))
+
+    # root = None
+    # x in tree
+    # x not in tree
+    def test_path_find_empty_bst(self):
+        lca_test = lca()
+
+        root = None
+        path = []
+        x = 4
+        self.assertFalse(lca_test.path_find(root, x, path))
+
+
+    def test_path_find(self):
+        lca_test = lca()
+
+        root = Node(8) 
+        root.left = Node(3) 
+        root.right = Node(10)  
+        root.left.left = Node(1)
+        root.left.right = Node(6)
+        root.left.right.right = Node(7)
+        root.left.right.left = Node(4)
+        root.right.right = Node(14)
+        root.right.right.left = Node(13)
+
+        path = []
+        x = 4
+
+        self.assertTrue(lca_test.path_find(root, x, path))
 
 
 
